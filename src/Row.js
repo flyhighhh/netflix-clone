881dcf0fcaa5e45146e234bbdda0c8489f3dfa18
flyhighhh <minsun0724@gmail.com>
2022-03-08 23:21:21 +0900

    Initial commit

---
+++ src/Row.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+//컴포넌트 파일은 파일명 대문자로
+
+function Row({ title }) {
+    const [movies, setMovies] = useState([]);
+    
+    //
+
+  return (
+    <div>
+        <h2>{title}</h2>
+
+        {/* container -> posters */}
+        
+    </div>
+  )
+}
+
+export default Row
---
+++ src/requests.js
@@ -0,0 +1,15 @@
+//tmdb에서 받은 api키
+const APIKEY = "da5ed7b2c261b57dc025302826cde6c7";
+
+const requests={
+    fetchTrending: `/trending/all/week?api_key={API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key={API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key={API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key={API_KEY}&with_genres=28`,
+    fetchComedyMobvies: `/discover/movie?api_key={API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key={API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key={API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key={API_KEY}&with_genres=99`,
+}
+
+export default requests;
